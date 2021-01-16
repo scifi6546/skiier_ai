dc85a2bca6c980af3f5c401c8f2eff0c49242ca7
Nick Alexeev <nickalexeev@protonmail.com>
2021-01-16 08:51:08 -0900

    initial

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+use nalgebra::Vector2;
+use petgraph::Graph;
+fn grid_graph(size: usize) -> Graph<Vector2<i32>, f32> {
+    todo!()
+}
+pub fn g() {}
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
