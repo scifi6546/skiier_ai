c5b78ccf058ae1ae3706f9bb0d4194eba94c449f
BuildTools <nickalexeev@protonmail.com>
2021-01-17 11:40:38 -0900

    got AI working

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod topography;
 use nalgebra::Vector2;
+use petgraph::algo::dijkstra;
 use petgraph::graph::DiGraph;
 use petgraph::prelude::NodeIndex;
 use petgraph::Graph;
@@ -9,12 +10,16 @@ struct Lift {
     end: Vector2<u32>,
 }
 struct World {
-    topo: Topography,
+    topography: Topography,
     lifts: Vec<Lift>,
 }
-fn grid_graph(topo: Topography) -> DiGraph<Vector2<i32>, f32> {
+struct GraphRepr {
+    graph: DiGraph<Vector2<i32>, u32>,
+    indicies: Vec<Vec<NodeIndex<u32>>>,
+}
+fn grid_graph(topo: &Topography, slope_function: fn(f32) -> u32) -> GraphRepr {
     let mut graph = Graph::new();
-    let idx: Vec<Vec<NodeIndex<u32>>> = (0..topo.dimensions.x)
+    let indicies: Vec<Vec<NodeIndex<u32>>> = (0..topo.dimensions.x)
         .map(|i| {
             (0..topo.dimensions.y)
                 .map(|j| graph.add_node(Vector2::new(i as i32, j as i32)))
@@ -26,39 +31,39 @@ fn grid_graph(topo: Topography) -> DiGraph<Vector2<i32>, f32> {
             let source = Vector2::new(i, j);
             if i >= 1 {
                 let end = Vector2::new(i - 1, j);
-                let slope = topo.slope(source, end);
+                let slope = slope_function(topo.slope(source, end));
 
                 graph.add_edge(
-                    idx[i as usize][j as usize],
-                    idx[i as usize - 1][j as usize],
+                    indicies[i as usize][j as usize],
+                    indicies[i as usize - 1][j as usize],
                     slope,
                 );
-                let slope = topo.slope(end, source);
+                let slope = slope_function(topo.slope(end, source));
                 graph.add_edge(
-                    idx[i as usize - 1][j as usize],
-                    idx[i as usize][j as usize],
-                    -1.0 * slope,
+                    indicies[i as usize - 1][j as usize],
+                    indicies[i as usize][j as usize],
+                    slope,
                 );
             }
             if j >= 1 {
                 let end = Vector2::new(i, j - 1);
-                let slope = topo.slope(source, end);
+                let slope = slope_function(topo.slope(source, end));
 
                 graph.add_edge(
-                    idx[i as usize][j as usize],
-                    idx[i as usize][j as usize - 1],
+                    indicies[i as usize][j as usize],
+                    indicies[i as usize][j as usize - 1],
                     slope,
                 );
-                let slope = topo.slope(end, source);
+                let slope = slope_function(topo.slope(end, source));
                 graph.add_edge(
-                    idx[i as usize][j as usize - 1],
-                    idx[i as usize][j as usize],
-                    -1.0 * slope,
+                    indicies[i as usize][j as usize - 1],
+                    indicies[i as usize][j as usize],
+                    slope,
                 );
             }
         }
     }
-    graph
+    GraphRepr { graph, indicies }
 }
 
 trait DecisionTreeNode {
@@ -96,24 +101,88 @@ trait DecisionTreeNode {
     }
 }
 struct Up {}
+impl Up {
+    fn slope(x: f32) -> u32 {
+        if x < 0.0 {
+            1000000
+        } else {
+            (x * 1.0) as u32
+        }
+    }
+}
 impl DecisionTreeNode for Up {
     fn children(&self) -> Vec<Box<dyn DecisionTreeNode>> {
         vec![Box::new(Up {}), Box::new(Down {})]
     }
     fn cost(&self, world: &World, position: Vector2<u32>) -> (f32, Vector2<u32>) {
-        (1.0, position)
+        let g = grid_graph(&world.topography, Self::slope);
+        let (cost, position) = world
+            .lifts
+            .iter()
+            .map(|lift| {
+                let start = g.indicies[lift.start.x as usize][lift.start.y as usize];
+                let end = g.indicies[lift.end.x as usize][lift.end.y as usize];
+                let path = dijkstra(&g.graph, start, Some(end), |x| 1);
+                let cost: u32 = path.iter().map(|(_, cost)| cost).sum();
+
+                (cost, lift.end)
+            })
+            .fold(
+                (u32::MAX, Vector2::new(0, 0)),
+                |(acc_cost, acc_lift), (x_cost, x_lift)| {
+                    if acc_cost < x_cost {
+                        (acc_cost, acc_lift)
+                    } else {
+                        (x_cost, x_lift)
+                    }
+                },
+            );
+
+        (cost as f32, position)
     }
     fn name(&self) -> String {
         "Up".to_string()
     }
 }
 struct Down {}
+impl Down {
+    fn slope(x: f32) -> u32 {
+        if x > 0.0 {
+            100000
+        } else {
+            (x.abs() * 1.0) as u32
+        }
+    }
+}
 impl DecisionTreeNode for Down {
     fn children(&self) -> Vec<Box<dyn DecisionTreeNode>> {
         vec![Box::new(Up {}), Box::new(Down {})]
     }
     fn cost(&self, world: &World, position: Vector2<u32>) -> (f32, Vector2<u32>) {
-        (10.0, position)
+        let g = grid_graph(&world.topography, Self::slope);
+        let (cost, position) = world
+            .lifts
+            .iter()
+            .map(|lift| {
+                let start = g.indicies[position.x as usize][position.y as usize];
+                let end = g.indicies[lift.start.x as usize][lift.start.y as usize];
+                let path = dijkstra(&g.graph, start, Some(end), |x| x.weight().clone());
+                let cost: u32 = path.iter().map(|(_, cost)| cost.clone().clone()).sum();
+
+                (cost, lift.end)
+            })
+            .fold(
+                (u32::MAX, Vector2::new(0, 0)),
+                |(acc_cost, acc_lift), (x_cost, x_lift)| {
+                    if acc_cost < x_cost {
+                        (acc_cost, acc_lift)
+                    } else {
+                        (x_cost, x_lift)
+                    }
+                },
+            );
+
+        (cost as f32, position)
     }
     fn name(&self) -> String {
         "Down".to_string()
@@ -122,8 +191,11 @@ impl DecisionTreeNode for Down {
 
 fn new_world() -> World {
     World {
-        topo: Topography::flat(Vector2::new(100, 100)),
-        lifts: vec![],
+        topography: Topography::cone(Vector2::new(100, 100), Vector2::new(50.0, 50.0), -1.0),
+        lifts: vec![Lift {
+            start: Vector2::new(0, 0),
+            end: Vector2::new(10, 10),
+        }],
     }
 }
 pub fn main() {
--- src/topography.rs
+++ src/topography.rs
@@ -2,7 +2,6 @@ use nalgebra::Vector2;
 #[derive(Clone)]
 pub struct Topography {
     pub height: Vec<f32>,
-    pub edges: Vec<f32>,
     pub dimensions: Vector2<u32>,
 }
 impl Topography {
@@ -11,26 +10,27 @@ impl Topography {
     }
 
     pub fn flat(dimensions: Vector2<u32>) -> Self {
-        let mut s = Self {
+        Self {
             height: (0..dimensions.x * dimensions.y).map(|_| 0.0).collect(),
-            edges: vec![],
             dimensions,
-        };
-        s.build_slope();
-        s
+        }
+    }
+    pub fn cone(dimensions: Vector2<u32>, center: Vector2<f32>, slope: f32) -> Self {
+        Self {
+            height: (0..dimensions.x * dimensions.y)
+                .map(|i| {
+                    let pos = Vector2::new((i / dimensions.y) as f32, (i % dimensions.y) as f32);
+                    ((pos.x - center.x).powi(2) + (pos.y - center.y).powi(2)).sqrt() * slope
+                })
+                .collect(),
+            dimensions,
+        }
     }
     fn get(&self, index: Vector2<u32>) -> f32 {
         let i = index.x * self.dimensions.y + index.y;
         self.height[i as usize]
     }
     pub fn slope(&self, start: Vector2<u32>, end: Vector2<u32>) -> f32 {
-        todo!()
-    }
-    /// Rebuilds Slope from self height
-    fn build_slope(&mut self) {
-        self.edges = vec![0.0; (self.dimensions.x as usize - 1) * (self.dimensions.y as usize - 1)];
-        for i in 0..self.dimensions.x - 1 {
-            for j in 0..self.dimensions.y - 1 {}
-        }
+        self.get(start) - self.get(end)
     }
 }
